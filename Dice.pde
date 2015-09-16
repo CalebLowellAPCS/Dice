5c22fe8297985c1948cc3e51d20ec3d20f2949d3
unknown <cakahookele@697-334-Andunie.sf.k12.internal>
2015-09-16 10:44:22 -0700

    ttotall really works forreals

--- Dice.pde
+++ Dice.pde
@@ -7,25 +7,13 @@ void setup()
 int rectx = 0;
 int recty = 0;
 int dots;
-float total1 = 0;
-float total2 = 0;
-float total3 = 0;
-float total4 = 0;
-float total5 = 0;
-float total6 = 0;
 
-
+int total = 0;
 
 void draw()
 {
-  background(0, 0, 0);
-  total1 = 0;
-  total2 = 0;
-  total3 = 0;
-  total4 = 0;
-  total5 = 0;
-  total6 = 0;
 
+  background(0, 0, 0);
 
   for (int recty = 0; recty <= 500; recty += 40) {
 
@@ -40,7 +28,7 @@ void draw()
 
   }
   
-  text("Total: "+ (int)((total1 * 1) + (total2 * 2) + (total3 * 3) + (total4 * 4) + (total5 * 5) + (total6 * 6)), 260, 530);
+  text("Total: " + (total), 260, 530);
   noLoop();
 }
 void mousePressed()
@@ -49,11 +37,9 @@ void mousePressed()
 }
 class Die //models one single dice cube
 {
-  //variable declarations here
+
   Die(int x, int y) //constructor
   {
-    roll();
-    show();
     rectx = x;
     recty = y;
   }
@@ -75,20 +61,20 @@ class Die //models one single dice cube
 
     if (dots == 1){
       
-      total1++;
+      total++;
       ellipse(rectx + 20, recty + 20, 5, 5);
 
     }
     if (dots == 2){
       
-      total2++;
+      total += 2;
       ellipse(rectx + 10, recty + 10, 5, 5);
       ellipse(rectx + 30, recty + 30, 5, 5);
 
     }
     if (dots == 3){
       
-      total3++;
+      total += 3;
       ellipse(rectx + 10, recty + 10, 5, 5);
       ellipse(rectx + 20, recty + 20, 5, 5);
       ellipse(rectx + 30, recty + 30, 5, 5);
@@ -96,7 +82,7 @@ class Die //models one single dice cube
     }
     if (dots == 4){
       
-      total4++;
+      total += 4;
       ellipse(rectx + 10, recty + 10, 5, 5);
       ellipse(rectx + 10, recty + 30, 5, 5);
       ellipse(rectx + 30, recty + 10, 5, 5);
@@ -106,7 +92,7 @@ class Die //models one single dice cube
     }
     if (dots == 5){
       
-      total5++;
+      total += 5;
       ellipse(rectx + 10, recty + 10, 5, 5);
       ellipse(rectx + 10, recty + 30, 5, 5);
       ellipse(rectx + 30, recty + 10, 5, 5);
@@ -117,7 +103,7 @@ class Die //models one single dice cube
     }
     if (dots == 6){
       
-      total6++;
+      total += 6;
       ellipse(rectx + 10, recty + 10, 5, 5);
       ellipse(rectx + 10, recty + 30, 5, 5);
       ellipse(rectx + 30, recty + 10, 5, 5);
--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -16,106 +16,120 @@ public class Dice extends PApplet {
 
 public void setup()
 {
-	size(520, 520);
-	noLoop();
+  size(520, 550);
+  noLoop();
 }
 
 int rectx = 0;
 int recty = 0;
 int dots;
 
+int total = 0;
+
 public void draw()
 {
-	background(0, 0, 0);
 
+  background(0, 0, 0);
 
-	for (int recty = 0; recty <= 500; recty += 40) {
+  for (int recty = 0; recty <= 500; recty += 40) {
 
-		for (int rectx = 0; rectx <= 500; rectx += 40) {
-			
+    for (int rectx = 0; rectx <= 500; rectx += 40) {
 
-			Die potato = new Die(rectx, recty);
-			potato.roll();
-			potato.show();
-		}
+      Die potato = new Die(rectx, recty);
+      
+      potato.roll();
+      potato.show();
+      
+    }
 
-	}
-	noLoop();
+  }
+  
+  text("Total: " + (dots), 260, 530);
+  noLoop();
 }
 public void mousePressed()
 {
-	redraw();
+  redraw();
 }
 class Die //models one single dice cube
 {
-	//variable declarations here
-	Die(int x, int y) //constructor
-	{
-		roll();
-		show();
-		rectx = x;
-		recty = y;
-	}
 
-	public void roll()
-	{
-	
-		dots = (int)(Math.random()*6 + 1);
+  Die(int x, int y) //constructor
+  {
+    rectx = x;
+    recty = y;
+  }
 
-	}
+  public void roll()
+  {
+  
+    dots = (int)(Math.random()*6 + 1);
 
-	public void show()
-	{
-		fill(0, 0, 0);
+  }
 
-		rect(rectx, recty, 40, 40);
+  public void show()
+  {
+    fill(0, 0, 0);
 
-		fill(255, 215, 0);
+    rect(rectx, recty, 40, 40);
 
-		if (dots == 1){
-		
-			ellipse(rectx + 20, recty + 20, 5, 5);
+    fill(255, 215, 0);
 
-		}
-		if (dots == 2){
-			ellipse(rectx + 10, recty + 10, 5, 5);
-			ellipse(rectx + 30, recty + 30, 5, 5);
+    if (dots == 1){
+      
+      total++;
+      ellipse(rectx + 20, recty + 20, 5, 5);
 
-		}
-		if (dots == 3){
-			ellipse(rectx + 10, recty + 10, 5, 5);
-			ellipse(rectx + 20, recty + 20, 5, 5);
-			ellipse(rectx + 30, recty + 30, 5, 5);
+    }
+    if (dots == 2){
+      
+      total += 2;
+      ellipse(rectx + 10, recty + 10, 5, 5);
+      ellipse(rectx + 30, recty + 30, 5, 5);
 
-		}
-		if (dots == 4){
-			ellipse(rectx + 10, recty + 10, 5, 5);
-			ellipse(rectx + 10, recty + 30, 5, 5);
-			ellipse(rectx + 30, recty + 10, 5, 5);
-			ellipse(rectx + 30, recty + 30, 5, 5);
+    }
+    if (dots == 3){
+      
+      total += 3;
+      ellipse(rectx + 10, recty + 10, 5, 5);
+      ellipse(rectx + 20, recty + 20, 5, 5);
+      ellipse(rectx + 30, recty + 30, 5, 5);
 
-
-		}
-		if (dots == 5){
-			ellipse(rectx + 10, recty + 10, 5, 5);
-			ellipse(rectx + 10, recty + 30, 5, 5);
-			ellipse(rectx + 30, recty + 10, 5, 5);
-			ellipse(rectx + 30, recty + 30, 5, 5);
-			ellipse(rectx + 20, recty + 20, 5, 5);
+    }
+    if (dots == 4){
+      
+      total += 4;
+      ellipse(rectx + 10, recty + 10, 5, 5);
+      ellipse(rectx + 10, recty + 30, 5, 5);
+      ellipse(rectx + 30, recty + 10, 5, 5);
+      ellipse(rectx + 30, recty + 30, 5, 5);
 
 
-		}
-		if (dots == 6){
-			ellipse(rectx + 10, recty + 10, 5, 5);
-			ellipse(rectx + 10, recty + 30, 5, 5);
-			ellipse(rectx + 30, recty + 10, 5, 5);
-			ellipse(rectx + 30, recty + 30, 5, 5);
-			ellipse(rectx + 10, recty + 20, 5, 5);
-			ellipse(rectx + 30, recty + 20, 5, 5);
+    }
+    if (dots == 5){
+      
+      total += 5;
+      ellipse(rectx + 10, recty + 10, 5, 5);
+      ellipse(rectx + 10, recty + 30, 5, 5);
+      ellipse(rectx + 30, recty + 10, 5, 5);
+      ellipse(rectx + 30, recty + 30, 5, 5);
+      ellipse(rectx + 20, recty + 20, 5, 5);
+
+
+    }
+    if (dots == 6){
+      
+      total += 6;
+      ellipse(rectx + 10, recty + 10, 5, 5);
+      ellipse(rectx + 10, recty + 30, 5, 5);
+      ellipse(rectx + 30, recty + 10, 5, 5);
+      ellipse(rectx + 30, recty + 30, 5, 5);
+      ellipse(rectx + 10, recty + 20, 5, 5);
+      ellipse(rectx + 30, recty + 20, 5, 5);
  
-		}
+    }
 
-	}
+  }
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Dice" };
